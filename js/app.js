8e332553c63fe9b6994b1bfe21abc5c724dd29f4
GoldbeardSea <timbusch1@gmail.com>
2018-04-09 16:21:57 -0700

    fixing errors in links, styles, js app

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
-var hours = [6, 7, 8 , 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+var hours = [6, 7, 8 , 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
 
-var firstPike {
+var firstP = document.getElementById('first');
+
+var firstPike = {
   minCust: 23,
   maxCust: 65,
   avgSale: 6.3,
-  customerArr: []
+  customerArr: [],
   salesByHour: [],
-  randCustHour: function randomCust(min, max) {
-    return Math.floor(Math.random() * (this.maxCust-minCust)+minCust);
+  customerCalc: 0,
+  randCustHour: function () {
+    this.customerCalc = Math.floor(Math.random() * (this.maxCust - this.minCust)+ this.minCust);
   },
-
-
-  
+};
+for ( i = 0, i > hours.length, i++); {
+var liEl = document.createElement('li');
+liEl.textContent = hours[i];
+firstP.appendChild(liEl);
 }
