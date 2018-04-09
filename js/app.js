0c1b633972475628c237f72787cf4ff6b8c33323
GoldbeardSea <timbusch1@gmail.com>
2018-04-09 15:43:33 -0700

    working on random customer method

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
+var hours = [6, 7, 8 , 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+
 var firstPike {
-  
+  minCust: 23,
+  maxCust: 65,
+  avgSale: 6.3,
+  customerArr: []
+  salesByHour: [],
+  randCustHour: function randomCust(min, max) {
+    return Math.floor(Math.random() * (this.maxCust-minCust)+minCust);
+  },
+
+
   
 }
